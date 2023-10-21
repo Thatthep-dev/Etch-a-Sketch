66e702f5609d4c6234e32b558e77dfecdaa99e2d
Thatthep <thatthep.dev@gmail.com>
2023-10-21 19:03:42 +0700

    Add resetButton(set background to white)

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// Board logic
-
 // <-- UI SECTION -->
 // Reference point
 const displayBoard = document.querySelector("#displayBoard");
+const resetButton = document.querySelector("#resetButton");
 
 // Create grid board
 function createGrid(row, column) {
@@ -11,16 +10,28 @@ function createGrid(row, column) {
     const newGridRow = document.createElement("div");
     newGridRow.classList.add("grid-row");
     displayBoard.appendChild(newGridRow);
-    console.log("row");
 
     for (let j = 0; j < column; j++) {
       // Create a new column for each iteration
       const newGridColumn = document.createElement("div");
       newGridColumn.classList.add("grid-column");
       newGridRow.appendChild(newGridColumn);
-      console.log("column");
+
+      // Add a click event listener to each grid column
+      newGridColumn.addEventListener("click", () => {
+        newGridColumn.style.backgroundColor = "black";
+      });
     }
   }
+  // After creating the grid, select all grid columns
+  gridColumnElements = document.querySelectorAll(".grid-column");
 }
+
+resetButton.addEventListener("click", () => {
+  // Change the background color of all grid cells to white
+  gridColumnElements.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
+});
 
 createGrid(4, 4);
